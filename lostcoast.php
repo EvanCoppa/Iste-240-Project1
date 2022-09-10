ece9f6badf82adfbd9aa6e4155f62f706df4e308
Evan Coppa <evancoppa@gmail.com>
2022-09-10 17:17:16 -0400

    added components

---
+++ lostcoast.php
@@ -0,0 +1,12 @@
+<div>
+    <div><p></p></div>
+    <div><img src="" alt=""></div>
+</div>
+<h1>California's</h1>
+<h1>Natioal Parks</h1>
+
+<div>
+    <div></div>
+    <div></div>
+    <div></div>
+</div>
