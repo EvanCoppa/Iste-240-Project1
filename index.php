e002de22300b5aa7f42b6fd79042c8c1a3613a2b
Evan Coppa <evancoppa@gmail.com>
2022-09-10 16:10:30 -0400

    added conponets

--- index.php
+++ index.php
@@ -1,11 +1,51 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <section>
+
+
+    </section>
+    <section>
+        <!-- Explore -->
+        <div>
+            <div>
+                <div>
+                    <h3></h3>
+                    <p></p>
+                </div>
+                <div><img src="" alt=""></div>
+            </div>
+            <div>
+                <div>
+                    <h3></h3>
+                    <p></p>
+                </div>
+                <div><img src="" alt=""></div>
+            </div>
+            <div>
+                <div>
+                    <h3></h3>
+                    <p></p>
+                </div>
+                <div><img src="" alt=""></div>
+            </div>
+            <div>
+                <div>
+                    <h3></h3>
+                    <p></p>
+                </div>
+                <div><img src="" alt=""></div>
+            </div>
+        </div>
+
+    </section>
 </body>
+
 </html>
